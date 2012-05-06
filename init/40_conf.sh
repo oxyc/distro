36cc94d575d9df88ae2e2734dcb52a7a38a076cc
Oskar Schöldström <public@oxy.fi>
2012-05-06 21:14:05 +0300

    Add some more configs

--- init/40_conf.sh
+++ init/40_conf.sh
@@ -1,7 +1,12 @@
 #/bin/bash
+
+qbt_conf="$HOME/.config/qBittorrent/qBittorrent.conf"
+vlc_conf="$HOME/.config/vlc/vlcrc"
+mimeinfo_conf="/usr/share/applications/mimeinfo.cache"
+defaults_conf="/usr/share/applications/defaults.list"
+
 # Crontab {{{
 
-# Run offlineimap every 5min
 jobs=(
   "*/5 * * * * offlineimap"
 )
@@ -17,41 +22,44 @@ if [ ${#array[*]} -gt 0 ]; then
     fi
   done
 fi
+unset jobs
 
 # }}}
 # VLC {{{
-config="$HOME/.config/vlc/vlcrc"
 
 if [ -e "$config" ]; then
   e_header "Configuring VLC"
 
   e_notify "Enable Minimal View" \
-    $(sed -iq 's/^\(qt-minimal-view=\).*$/\11/' "$config")
+    $(sed -iq 's/^\(qt-minimal-view=\).*$/\11/' "$vlc_conf")
   e_notify "Disable Privacy Ask" \
-    $(sed -iq 's/^\(qt-privacy-ask=\).*$/\10/' "$config")
+    $(sed -iq 's/^\(qt-privacy-ask=\).*$/\10/' "$vlc_conf")
   e_notify "Set Volume to 400%" \
-    $(sed -iq 's/^\(volume=\).*$/\1400/' "$config")
+    $(sed -iq 's/^\(volume=\).*$/\1400/' "$vlc_conf")
+
+  e_notify "Replace all totem usage with vlc" \
+    $(sudo sed -iq 's/totem\.desktop/vlc\.desktop/g' "$mimeinfo_conf" \
+      && sudo sed -iq 's/totem\.desktop/vlc\.desktop/g' "$defaults_conf")
 fi
 
 # }}}
 # qBittorrent {{{
 
-config="$HOME/.config/qBittorrent/qBittorrent.conf"
 
 if [ -e "$config" ]; then
   e_header "Configuring qBittorrent"
 
-  DownloadsSavePath=$(escape "${HOME}/Downloads")
-  DownloadsTempPath=$(escape "${HOME}/Downloads/temp")
+  local DownloadsSavePath=$(escape "${HOME}/Downloads")
+  local DownloadsTempPath=$(escape "${HOME}/Downloads/temp")
 
   e_notify "Enable CloseToTray" \
-    $(sed -iq "s/^\\(General\\\CloseToTray=\\).*$/\1true/" "$config")
+    $(sed -iq "s/^\\(General\\\CloseToTray=\\).*$/\1true/" "$qbt_conf")
   e_notify "Set SavePath to $DownloadsSavePath" \
-    $(sed -iq "s/^\\(Downloads\\\SavePath=\\).*$/\1$DownloadsSavePath/" "$config")
+    $(sed -iq "s/^\\(Downloads\\\SavePath=\\).*$/\1$DownloadsSavePath/" "$qbt_conf")
   e_notify "Enable TempPath" \
-    $(sed -iq "s/^\\(Downloads\\\TempPathEnabled=\\).*$/\1true/" "$config")
+    $(sed -iq "s/^\\(Downloads\\\TempPathEnabled=\\).*$/\1true/" "$qbt_conf")
   e_notify "Set TempPath to $DownloadsTempPath" \
-    $(sed -iq "s/^\\(Downloads\\\TempPath=\\).*$/\1$DownloadsTempPath/" "$config")
+    $(sed -iq "s/^\\(Downloads\\\TempPath=\\).*$/\1$DownloadsTempPath/" "$qbt_conf")
 fi
 
 # }}}
@@ -64,25 +72,47 @@ if [ "$(type -P gconftool-2)" ]; then
   # http://askubuntu.com/questions/35890/how-to-programmatically-swap-the-caps-lock-and-esc-keys
   e_notify "Map caps to escape" \
     $(gconftool-2 --set /desktop/gnome/peripherals/keyboard/kbd/options \
-      --type list --list-type string \
-      '[caps<tab>caps:swapescape]')
+        --type list --list-type string \
+        '[caps<tab>caps:swapescape]')
 
   # Workspace keyboard shortcuts
   e_notify "Map move_to_workspace_up to <Primary>XF86Back" \
     $(gconftool-2 --set /apps/metacity/window_keybindings/move_to_workspace_up \
-      --type string '<Primary>XF86Back')
+        --type string '<Primary>XF86Back')
 
   e_notify "Map move_to_workspace_down to <Primary>XF86Forward" \
     $(gconftool-2 --set /apps/metacity/window_keybindings/move_to_workspace_down \
-      --type string '<Primary>XF86Forward')
+        --type string '<Primary>XF86Forward')
 
   e_notify "Map switch_to_workspace_up to XF86Back" \
     $(gconftool-2 --set /apps/metacity/global_keybindings/switch_to_workspace_up \
-      --type string 'XF86Back')
+        --type string 'XF86Back')
 
   e_notify "Map switch_to_workspace_down to XF86Forward" \
     $(gconftool-2 --set /apps/metacity/global_keybindings/switch_to_workspace_down \
-      --type string 'XF86Forward')
+        --type string 'XF86Forward')
+
+  if [ "$(type -P google-chrome)" ]; then
+    e_notify "Use chrome as default browser" \
+      $(gconftool-2 --set /desktop/gnome/applications/browser/exec \
+          --type string $(which google-chrome))
+  fi
+
+  if [ "$(type -P marlin)" ]; then
+    e_notify "Use marlin as default file browser" \
+      $(gconftool-2 --set /desktop/gnome/applications/component_viewer/exec \
+          --type string 'marlin %s')
+  fi
+
+  if [ "$(type -P urxvt)" ]; then
+    e_notify "Use urxvt as default terminal" \
+      $(gconftool-2 --set /desktop/gnome/applications/terminal/exec \
+          --type string 'urxvt' \
+        && gconftool-2 --set /desktop/gnome/applications/terminal/exec_arg \
+        --type string '-e')
+  fi
 fi
 
 # }}}
+
+unset qbt_conf vlc_conf mimeinfo_conf defaults_conf

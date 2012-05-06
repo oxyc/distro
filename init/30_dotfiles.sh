4ab0fafc2f7acb3731bf62c4539d46c0b52afc86
Oskar Schöldström <public@oxy.fi>
2012-05-06 21:40:51 +0300

    Download dotfiles and vimrc

---
+++ init/30_dotfiles.sh
@@ -0,0 +1,4 @@
+
+sudo cpan Term::ExtendedColor
+cd && wget https://raw.github.com/oxyc/dotfiles/master/install.sh -O - | pipe=1 bash
+cd && wget https://raw.github.com/oxyc/vimrc/master/install.sh -O - | bash
